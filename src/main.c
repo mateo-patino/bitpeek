1f2c0d3055da37aea47a09172661dc329e5227ce
Mateo Patino <mateopatinodev@gmail.com>
2026-06-25 00:19:36 -0500

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+* Entry point for pcalc.
+*/
+
+int main(int argc, char** argv) {
+    
+    /*
+    * TODO:
+    * 1) Take argv and produce a token array
+    * 2) Build an AST with the token array
+    * 3) Perform semantic analysis (i.e. does the math make sense, type checking and errors)
+    * 4) Traverse the AST to evaluate
+    * 5) Output the result
+    */
+
+    return 0;
+}
