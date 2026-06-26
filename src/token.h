17d0ae9e135f32247c0921fba2ce927c36feb7c7
Mateo Patino <mateopatinodev@gmail.com>
2026-06-25 20:43:46 -0500

    Initial token_t, operand_t and number_t definitions

---
+++ src/token.h
@@ -0,0 +1,44 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <stdint.h>
+
+/* Token types */
+typedef enum {
+  OPERAND,
+  NUMBER,
+  LPAREN,
+  RPAREN
+} token_type;
+
+/* Supported operations */
+typedef enum {
+    ADD,
+    SUB,
+    MUL,
+    DIV
+} operation_type;
+
+
+/* Token struct; serves as a wrapper around number_t and operand_t */
+typedef struct {
+    token_type type;
+    void *obj;
+} token_t;
+
+/* Number data type */
+typedef uint64_t value_t;
+typedef struct {
+    value_t value;
+    char base;
+} number_t;
+
+/* Operand data type */
+typedef struct {
+    char arity;
+    char precedence;
+    operation_type op;
+} operand_t;
+
+#endif
+
