24e108301b6ccd7d65f7863ebd527eb911ceab54
Mateo Patino <mateopatinodev@gmail.com>
2026-06-27 15:08:27 -0500

    Added operand_t constructor

--- src/token.c
+++ src/token.c
@@ -13,3 +13,15 @@ number_t *init_number(value_t value, int base) {
     
     return obj;
 }
+
+operand_t *init_operand(operation_type type) {
+    operand_t *op = malloc(sizeof(operand_t));
+    if (!op) {
+        return NULL;
+    }
+    op->op = type;
+    op->arity = op_arity[type];
+    op->precedence = op_precedence[type];
+
+    return op;
+}
