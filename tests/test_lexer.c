d4fca16168347576030378aaadac82410041d194
Mateo Patino <mateopatinodev@gmail.com>
2026-07-02 17:00:20 -0500

    Add unit test for tokenization of valid arithmetic operation strings

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -4,16 +4,27 @@
 #include "token.h"
 #include "lexer.h"
 
+#include <stdbool.h>
+
 bool test_valid_arithmetic_op_tokenizer(void) {
     operation_type op;
-    bool result;
+    bool retval;
 
-    ASSERT_TRUE(is_operation("add", &op) == ADD);
-    
-    ASSERT_TRUE(is_operation("sub", &op) == SUB);
+    retval = is_operation("add", &op);
+    ASSERT_TRUE(retval);
+    ASSERT_TRUE(op == ADD);
 
-    ASSERT_TRUE(is_operation("mul", &op) == MUL);
+    retval = is_operation("sub", &op);
+    ASSERT_TRUE(retval);
+    ASSERT_TRUE(op == SUB);
 
-    ASSERT_TRUE(is_operation("div", &op) == DIV);
+    retval = is_operation("mul", &op);
+    ASSERT_TRUE(retval);
+    ASSERT_TRUE(op == MUL);
 
+    retval = is_operation("div", &op);
+    ASSERT_TRUE(retval);
+    ASSERT_TRUE(op == DIV);
+
+    return true;
 }
--- tests/test_lexer.h
+++ tests/test_lexer.h
@@ -1,4 +1,6 @@
 #include <stdbool.h>
 
-
+/*
+* Tests tokenization of valid srtrings for arithmetic ops 'add', 'sub', 'mul', and 'div'.
+*/
 bool test_valid_arithmetic_op_tokenizer(void);
