2779493df98035b02c4cd0801d51d636ac58f1aa
Mateo Patino <mateopatinodev@gmail.com>
2026-07-03 00:28:34 -0500

    Add test runner

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -5,6 +5,32 @@
 #include "lexer.h"
 
 #include <stdbool.h>
+#include <errno.h>
+
+#define ARRAY_LEN(a) (sizeof(a) / sizeof((a)[0]))
+
+typedef bool (*lexer_test_t)(void);
+static const lexer_test_t lexer_tests[] = {
+    test_valid_arithmetic_op_tokenizer,
+    test_invalid_arithmetic_op_tokenizer,
+    test_valid_decimal_tokenizer,
+    test_invalid_decimal_tokenizer
+};
+
+
+/* Runner for the entire module */
+int run_lexer_tests(int *total_ran) {
+    size_t test_count = ARRAY_LEN(lexer_tests);
+    int pass = 0;
+    for (size_t i = 0; i < test_count; i++) {
+        if (lexer_tests[i]()) {
+            pass++;
+        }
+    }
+    if (total_ran) { *total_ran = (int)test_count; }
+    return pass;
+}
+
 
 bool test_valid_arithmetic_op_tokenizer(void) {
     operation_type op;
@@ -39,9 +65,6 @@ bool test_invalid_arithmetic_op_tokenizer(void) {
     ASSERT_TRUE(!is_operation("\\", NULL));
     ASSERT_TRUE(!is_operation("dog", NULL));
     ASSERT_TRUE(!is_operation("0x10", NULL));
-    ASSERT_TRUE(!is_operation("+add", NULL));
-    ASSERT_TRUE(!is_operation("add+", NULL));
-    ASSERT_TRUE(!is_operation("addd", NULL));
     ASSERT_TRUE(!is_operation("adds", NULL));
     ASSERT_TRUE(!is_operation(NULL, NULL));
 
@@ -54,6 +77,10 @@ bool test_valid_decimal_tokenizer(void) {
      value_t val;
 
      ASSERT_TRUE(is_number("10", &base, &val));
+     ASSERT_EQ_INT(10, base);
+     ASSERT_EQ_VALUE_T((value_t)10, val);
+     
+     ASSERT_TRUE(is_number("  10  ", &base, &val));
      ASSERT_EQ_INT(10, base);
      ASSERT_EQ_VALUE_T((value_t)10, val);
 
@@ -73,10 +100,28 @@ bool test_valid_decimal_tokenizer(void) {
      ASSERT_EQ_INT(10, base);
      ASSERT_EQ_VALUE_T((value_t)123456789, val);
 
+     errno = 0;
      ASSERT_TRUE(is_number("18446744073709551615", &base, &val));
+     ASSERT_TRUE(errno == 0);
      ASSERT_EQ_INT(10, base);
      ASSERT_EQ_VALUE_T((value_t)18446744073709551615ULL, val);
  
      return true;
+}
+
+
+bool test_invalid_decimal_tokenizer(void) {
+    ASSERT_TRUE(!is_number("-10", NULL, NULL));
+    ASSERT_TRUE(!is_number("10-", NULL, NULL));
+    ASSERT_TRUE(!is_number("10 -", NULL, NULL));
+    ASSERT_TRUE(!is_number("string", NULL, NULL));
+    ASSERT_TRUE(!is_number("", NULL, NULL));
+    ASSERT_TRUE(!is_number("-123456789", NULL, NULL));
+    ASSERT_TRUE(!is_number("18446744073709551616", NULL, NULL));
+    ASSERT_TRUE(errno == ERANGE);
+    ASSERT_TRUE(!is_number("-18446744073709551614", NULL, NULL));
+    ASSERT_TRUE(!is_number(NULL, NULL, NULL));
+
+    return true;
 }
 
--- tests/test_lexer.h
+++ tests/test_lexer.h
@@ -1,18 +1,35 @@
 #include <stdbool.h>
 
 /*
+* Calls all of the unit and integration tests for testing the lexer. 
+*
+* Returns the number of tests passed, and if 'total_ran' is not NULL, it writes the total
+* number of tests ran there.
+*/
+int run_lexer_tests(int *total_ran);
+
+
+/*
 * Tests tokenization of valid srtrings for arithmetic ops 'add', 'sub', 'mul', and 'div'.
 */
 bool test_valid_arithmetic_op_tokenizer(void);
+
 
 /*
 * Tests tokenization of invalid srtrings for arithmetic ops 'add', 'sub', 'mul', and 'div'.
 */
 bool test_invalid_arithmetic_op_tokenizer(void);
 
+
 /*
-* Tests tokenization of valid numbers in decimal base.
+* tests tokenization of valid numbers in decimal base.
 */
 bool test_valid_decimal_tokenizer(void);
+
+
+/*
+* tests tokenization of invalid numbers in decimal base.
+*/
+bool test_invalid_decimal_tokenizer(void);
 
 
