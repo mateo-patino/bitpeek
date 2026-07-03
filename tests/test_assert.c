84261bfb637265f6a758dbd2956ab72e7e36e578
Mateo Patino <mateopatinodev@gmail.com>
2026-07-03 17:40:56 -0500

    Improved pretty printing format

--- tests/test_assert.c
+++ tests/test_assert.c
@@ -6,33 +6,33 @@
 
 
 void assert_true_failed(const char *file_name, int line, const char *func, const char *expr) {
-    fprintf(stderr, ANSI_RED BOLD "TEST FAILED" ANSI_RESET "\n");
-    fprintf(stderr, "Assertion failed: %s\n", expr);
+    fprintf(stderr, BOLD "%s " ANSI_RED "FAILED" ANSI_RESET "\n", func);
+    fprintf(stderr, "   Assertion failed: %s\n", expr);
     fprintf(stderr, "   at %s:%i\n", file_name, line);
     fprintf(stderr, "   in %s\n", func);
 }
 
 
 void assert_eq_int_failed(int exp, int rec, const char *file_name, int line, const char* func) {
-    fprintf(stderr, ANSI_RED BOLD "TEST FAILED" ANSI_RESET "\n");
-    fprintf(stderr, "Expected: %i Received: " BOLD "%i" ANSI_RESET "\n",  exp, rec);
+    fprintf(stderr, BOLD "%s " ANSI_RED "FAILED" ANSI_RESET "\n", func);
+    fprintf(stderr, "   Expected: %i Received: " BOLD "%i" ANSI_RESET "\n",  exp, rec);
     fprintf(stderr, "   at %s:%i\n", file_name, line);
     fprintf(stderr, "   in %s\n", func);
 }
 
 
 void assert_eq_number_t_failed(const number_t *exp, const number_t *rec, const char *file_name, int line, const char *func) {
-    fprintf(stderr, ANSI_RED BOLD "TEST FAILED" ANSI_RESET "\n");
-    fprintf(stderr, "Expected: number_t(val=%" PRIu64 ", base=%i)\n", exp->value, exp->base);
-    fprintf(stderr, "Received:" BOLD "number_t(val=%" PRIu64 ", base=%i)" ANSI_RESET "\n", rec->value, rec->base);
+    fprintf(stderr, BOLD "%s " ANSI_RED "FAILED" ANSI_RESET "\n", func);
+    fprintf(stderr, "   Expected: number_t(val=%" PRIu64 ", base=%i)\n", exp->value, exp->base);
+    fprintf(stderr, "   Received:" BOLD "number_t(val=%" PRIu64 ", base=%i)" ANSI_RESET "\n", rec->value, rec->base);
     fprintf(stderr, "   at %s:%i\n", file_name, line);
     fprintf(stderr, "   in %s\n", func);
 }
 
 
 void assert_eq_value_t_failed(value_t exp, value_t rec, const char *file_name, int line, const char *func) {
-    fprintf(stderr, ANSI_RED BOLD "TEST FAILED" ANSI_RESET "\n");
-    fprintf(stderr, "Expected: %" PRIu64 " Received: " BOLD "%" PRIu64 ANSI_RESET "\n",  exp, rec);
+    fprintf(stderr, BOLD "%s " ANSI_RED "FAILED" ANSI_RESET "\n", func);
+    fprintf(stderr,     "Expected: %" PRIu64 " Received: " BOLD "%" PRIu64 ANSI_RESET "\n",  exp, rec);
     fprintf(stderr, "   at %s:%i\n", file_name, line);
     fprintf(stderr, "   in %s\n", func);
 }
