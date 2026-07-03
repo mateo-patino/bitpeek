2ed78f46c6654a28d1bfb40e5b6e8b1e6efafbaf
Mateo Patino <mateopatinodev@gmail.com>
2026-07-03 17:43:28 -0500

    Add newline and stdout flushing

--- tests/test_main.c
+++ tests/test_main.c
@@ -7,11 +7,17 @@
 
 int main(void) {
 
+    fprintf(stdout, "\n");
+    fflush(stdout);
     fprintf(stdout, BOLD LINE "LEXER TESTS" LINE ANSI_RESET "\n"); 
-    int total_lexer = 0;
-    int pass_lexer = run_lexer_tests(&total_lexer);
-    fprintf(stdout, "Lexer Results: %i total, %i pass, %i fail\n", total_lexer, 
-            pass_lexer, total_lexer - pass_lexer);
+    fflush(stdout);
+
+    int total_lex = 0;
+    int crashes_lex = 0;
+    int pass_lex = run_lexer_tests(&total_lex, &crashes_lex);
+    fprintf(stdout, BOLD "Lexer Results: %i total, %i pass, %i fail (%i crash)" ANSI_RESET "\n", total_lex, 
+            pass_lex, total_lex - pass_lex, crashes_lex);
+    fflush(stdout);
 
     return 0;
 }
